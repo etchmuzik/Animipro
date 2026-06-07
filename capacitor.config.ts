500ad413e6a2956215b2e45ff86202fc12c5441c
Beyond Tech <beyondtecheg@gmail.com>
2026-06-07 22:26:22 +0300

    feat(mobile): init Capacitor with webDir fallback shell

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'online.animipro.app',
+  appName: 'Animipro',
+  webDir: 'mobile-shell'
+};
+
+export default config;
